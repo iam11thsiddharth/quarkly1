80960dab2c9f8d60107676ff3b4020a653823c44
iam11thsiddharth <32956219+iam11thsiddharth@users.noreply.github.com>
2021-07-04 20:52:38 +0530

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60e1d1b4a4e264001e65363c",
+				"60e1d1b4a4e264001e65363e"
+			]
+		},
+		"60e1d1b4a4e264001e65363c": {
+			"id": "60e1d1b4a4e264001e65363c",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60e1d1b4a4e264001e65363e": {
+			"id": "60e1d1b4a4e264001e65363e",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
